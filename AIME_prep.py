04cb7a470240b92e18830c86db5b841c8d067fd3
Huy Dang <xhuydng@gmail.com>
2025-09-28 22:14:28 +0700

    added portion sizes

--- AIME_prep.py
+++ AIME_prep.py
@@ -9,21 +9,21 @@ path = kagglehub.dataset_download("hemishveeraboina/aime-problem-set-1983-2024")
 csv_path = os.path.join(path, "AIME_Dataset_1983_2024.csv")   # replace with real filename
 df = pd.read_csv(csv_path)
 
-# Train split: 2013–2018
-train_df = df[df["Year"].between(2020,2022)]
+# Train split: 2020–2022
+train_df = df[df["Year"].between(2020, 2022)]
 
-# Val split: 2019–2021
-val_df = df[df["Year"]== 2023]
+# Val split: 2023
+val_df = df[df["Year"] == 2023]
 
-# Test split: 2022–2024
+# Test split: 2024
 test_df = df[df["Year"] == 2024]
 
 # Print number of problems
-total_samples = len(train_df)+len(val_df)+len(test_df)
-print("Total samples", total_samples)
-print("Train sample portion:", len(train_df)*100/total_samples)
-print("Validation portion:", len(val_df)*100/total_samples)
-print("Test portion:", (len(test_df)*100/total_samples))
+total_samples = len(train_df) + len(val_df) + len(test_df)
+print("Total samples:", total_samples)
+print("Train samples:", len(train_df), f"({round(len(train_df)*100/total_samples, 2)}%)")
+print("Validation samples:", len(val_df), f"({round(len(val_df)*100/total_samples, 2)}%)")
+print("Test samples:", len(test_df), f"({round(len(test_df)*100/total_samples, 2)}%)")
 
 # (Optional) Save them
 train_df.to_csv("AIME/AIME_train.csv", index=False)
